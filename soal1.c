c5d9768bd6fa149099890edfd35d2523bd4df548
Unknown <ghifarozarahmadiana@yahoo.com>
2017-11-15 04:08:12 +0700

    ganti dirpath

--- soal1.c
+++ soal1.c
@@ -8,7 +8,7 @@
 #include <errno.h>
 #include <sys/time.h>
 
-static const char *dirpath = "/home/administrator/Documents";
+static const char *dirpath = "/home/ghifarozarrr/Documents";
 
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
@@ -17,14 +17,12 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
 	sprintf(fpath,"%s%s",dirpath,path);
 	res = lstat(fpath, stbuf);
 
-	if (res == -1)
-		return -errno;
+	if (res == -1) return -errno;
 
 	return 0;
 }
 
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-		       off_t offset, struct fuse_file_info *fi)
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
   char fpath[1000];
 	if(strcmp(path,"/") == 0)
@@ -42,10 +40,10 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	(void) fi;
 
 	dp = opendir(fpath);
-	if (dp == NULL)
-		return -errno;
+	if (dp == NULL) return -errno;
 
-	while ((de = readdir(dp)) != NULL) {
+	while ((de = readdir(dp)) != NULL) 
+	{
 		struct stat st;
 		memset(&st, 0, sizeof(st));
 		st.st_ino = de->d_ino;
@@ -58,8 +56,7 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	return 0;
 }
 
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
-		    struct fuse_file_info *fi)
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
   char fpath[1000];
 	if(strcmp(path,"/") == 0)
@@ -69,7 +66,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 	}
 	else sprintf(fpath, "%s%s",dirpath,path);
 	int res = 0;
-  int fd = 0 ;
+	int fd = 0 ;
 
 	(void) fi;
 	fd = open(fpath, O_RDONLY);
@@ -84,7 +81,8 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 	return res;
 }
 
-static struct fuse_operations xmp_oper = {
+static struct fuse_operations xmp_oper = 
+{
 	.getattr	= xmp_getattr,
 	.readdir	= xmp_readdir,
 	.read		= xmp_read,
