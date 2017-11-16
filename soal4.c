cc0eda55c4bcae30699ee078b43dc55e760898e5
Rahandi <rahandi16@mhs.if.its.ac.id>
2017-11-16 19:24:33 +0700

    +E18_mknod, +E18_chmod, +E18_open

--- soal4.c
+++ soal4.c
@@ -42,6 +42,40 @@ static int E18_getdir(const char *path, fuse_dirh_t h, fuse_dirfil_t filler){
     return res;
 }
 
+static int E18_mknod(const char *path, mode_t mode, dev_t rdev){
+    int result;
+    char fullpath[100];
+    sprintf(fullpath, "%s%s", dirpath, path);
+    result = mknod(fullpath, mode, rdev);
+    if(result == -1){
+        return -errno;
+    }
+    return 0;
+}
+
+static int E18_chmod(const char *path, mode_t mode){
+    int result;
+    char fullpath[1000];
+    sprintf(fullpath, "%s%s", dirpath, path);
+    result = chmod(fullpath, mode);
+    if(result == -1){
+        return -errno;
+    }
+    return 0;
+}
+
+static int E18_open(const char *path, int flags){
+    int result;
+    char fullpath[1000];
+    sprintf(fullpath, "%s%s", dirpath, path);
+    result = open(fullpath, flags);
+    if(result == -1){
+        return -errno;
+    }
+    close(result);
+    return 0;
+}
+
 static struct fuse_operations E18_oper = {
     .getattr    =   E18_getattr,
     .getdir     =   E18_getdir,
