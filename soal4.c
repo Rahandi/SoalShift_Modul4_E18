d3e310da94c8eaecd867c1cf33ae35362b31e474
Rahandi <rahandi16@mhs.if.its.ac.id>
2017-11-16 19:18:34 +0700

    +E18_getattr

--- soal4.c
+++ soal4.c
@@ -11,6 +11,17 @@
 
 static const char *dirpath = "/home/randi/Downloads";
 
+static int E18_getattr(const char *path, struct stat *stbuf){
+    int result;
+    char fullpath[100];
+    sprintf(fullpath, "%s%s". dirpath, path);
+    result = lstat(fullpath, stbuf);
+    if(res == -1){
+        return -errno;
+    }
+    return 0;
+}
+
 static struct fuse_operations E18_oper = {
     .getattr    =   E18_getattr,
     .getdir     =   E18_getdir,
